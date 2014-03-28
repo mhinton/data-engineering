fc6cb59cc014cdd64c84d4d33a72e02947dcf0fc
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-28 15:53:32 -0500

    Add upload action and form

--- app/controllers/data_import_controller.rb
+++ app/controllers/data_import_controller.rb
@@ -1,4 +1,9 @@
 class DataImportController < ApplicationController
   def index
   end
+
+  def upload
+    data_file = params['import']
+    # #<ActionDispatch::Http::UploadedFile:0x007fb7f21fa2f0 @tempfile=#<Tempfile:/var/folders/b_/9k6mslj51vqfjym9ryvqpqkm0000gn/T/RackMultipart20140328-27920-w6otgy>, @original_filename="example_input.tab", @content_type="application/octet-stream", @headers="Content-Disposition: form-data; name=\"import\"; filename=\"example_input.tab\"\r\nContent-Type: application/octet-stream\r\n">
+  end
 end
