083612bc3d92b63bdfd1611fec63b878aa0887b4
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-28 15:53:14 -0500

    Add post route for upload action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ LSDataImport::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  post "data_import/upload" => "data_import#upload"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "data_import#index"
