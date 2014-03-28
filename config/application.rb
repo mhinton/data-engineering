fc0c55b0b83523ce16f1bbcf463361ed999dfcba
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-28 11:09:53 -0500

    Setup test generators config

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +23,16 @@ module LSDataImport
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Test framework
+    config.generators.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: true,
+      request_specs: false
+    config.generators.fixture_replacement :factory_girl, dir: "spec/factories"
+
   end
 end
