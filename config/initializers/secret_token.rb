037a55c8fa10ffa4514e4048898d5ff09303f7b1
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-28 10:34:53 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LSDataImport::Application.config.secret_key_base = '33210ebf1b87d665f268d568df61dd2d8e2d1da88e5da76418982a3b627e6d55129ee0e04268d98d3224877d243f0fa380e3367127d66b4deb057d3e0d6369e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LSDataImport::Application.config.session_store :cookie_store, key: '_LSDataImport_session'
