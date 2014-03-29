5d9edced4030b1697a1e2785076093654563a65a
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-29 11:12:50 -0500

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328202014) do
+ActiveRecord::Schema.define(version: 20140328210637) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20140328202014) do
   end
 
   add_index "customers", ["name"], name: "index_customers_on_name"
+
+  create_table "data_files", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "upload_file_name"
+    t.string   "upload_content_type"
+    t.integer  "upload_file_size"
+    t.datetime "upload_updated_at"
+  end
 
   create_table "items", force: true do |t|
     t.integer  "merchant_id",                         null: false
