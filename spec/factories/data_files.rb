438e66bdbeeb5e822d244e47606e5cade646dd5a
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-29 16:16:38 -0500

    Remove old column

--- spec/factories/data_files.rb
+++ spec/factories/data_files.rb
@@ -3,6 +3,5 @@ require 'faker'
 FactoryGirl.define do
   factory :data_file do
     upload { Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/fixtures/example_input.tab") }
-    uploaded_at { DateTime.now }
   end
 end
