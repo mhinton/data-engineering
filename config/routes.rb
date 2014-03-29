d9721eb75253d4a42406a1778d90ec2cacfa8ed9
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-29 14:06:19 -0500

    Fix root_path controller name

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ LSDataImport::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root "data_import#new"
+  root "data_files#new"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
