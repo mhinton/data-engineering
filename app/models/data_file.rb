ac326f54f3c5c02bc4d98f5e393501824657615f
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-29 11:12:38 -0500

    Configure paperclip and add the required validation

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,6 +1,5 @@
 class DataFile < ActiveRecord::Base
-  has_attached_file :upload, url: "/public/uploads/:attachment_:id"
-  # validates_attachment_content_type :upload, :content_type => ["application/octet-stream", "text/plain"]
+  has_attached_file :upload, url: "/uploads/:attachment_:id"
   validates_attachment :upload, presence: true, content_type: { content_type: ["application/octet-stream", "text/plain"] }
   validates_attachment_file_name :upload, :matches => [/tab\Z/]
 end
