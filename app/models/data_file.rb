eb61160b4db54ff447f7dfa5f0df9c0d220866d1
Matthew Hinton <matthew.hinton@commongroundpublishing.com>
2014-03-30 12:18:18 -0500

    Add the orders have_many association

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,4 +1,5 @@
 class DataFile < ActiveRecord::Base
+  has_many :orders, inverse_of: :data_file
   has_attached_file :upload, url: "/uploads/:attachment_:id"
   validates_attachment :upload, presence: true, content_type: { content_type: ["application/octet-stream", "text/plain"] }
   validates_attachment_file_name :upload, :matches => [/tab\Z/]
--- spec/models/data_file_spec.rb
+++ spec/models/data_file_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe DataFile do
+  it { should have_many(:orders) }
   it { should have_attached_file(:upload) }
   it { should validate_attachment_presence(:upload) }
   it { should validate_attachment_content_type(:upload).
